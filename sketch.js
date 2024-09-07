a37330fac3c7aba0a90b25035d203d4a486e6ab7
TIEN TRUONG <ttt21b@fsu.edu>
2024-09-07 14:53:44 -0400

    yippee i got 2 shapes

--- sketch.js
+++ sketch.js
@@ -4,6 +4,14 @@ let x1Speed = 3;  // speed in X direction
 let y1Speed = 7; // speed in Y direction
 let d1 = 20;      // diameter of the ball
 
+let x2 = 100;     // x position of the ball
+let y2 = 0;       // y position of the ball
+let x2Speed = 3;  // speed in X direction
+let y2Speed = 10; // speed in Y direction
+let d2 = 20;      // diameter of the ball
+  // if x hits the right side or the left side of the canvas,
+// switch directions...
+
 function setup() {
   createCanvas(400, 400);
 }
@@ -25,4 +33,19 @@ function draw() {
   y1 = y1 + y1Speed;
 
   ellipse(x1, y1, d1);
+
+
+  if (x2 > width || x2 < 0) {
+    x2Speed = x2Speed * -1;  
+  }
+  x2 = x2 + x2Speed; // adding a negative xSpeed *decreases* x, right?
+  
+  // same as above, but for the top and bottom of the canvas
+  if (y2 > width || y2 < 0) {
+    y2Speed = y2Speed * -1;  
+  }
+  y2 = y2 + y2Speed;
+
+  ellipse(x2, y2, d2);
+
 }
