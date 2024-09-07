bb69e51748206aa3000c1eed2c05727329535c1c
TIEN TRUONG <ttt21b@fsu.edu>
2024-09-07 14:31:19 -0400

    i changed the variables

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
-let x = 100;     // x position of the ball
-let y = 0;       // y position of the ball
-let xSpeed = 3;  // speed in X direction
-let ySpeed = 7; // speed in Y direction
-let d = 20;      // diameter of the ball
+let x1 = 100;     // x position of the ball
+let y1 = 0;       // y position of the ball
+let x1Speed = 3;  // speed in X direction
+let y1Speed = 7; // speed in Y direction
+let d1 = 20;      // diameter of the ball
 
 function setup() {
   createCanvas(400, 400);
@@ -13,16 +13,16 @@ function draw() {
  
   // if x hits the right side or the left side of the canvas,
   // switch directions...
-  if (x > width || x < 0) {
-    xSpeed = xSpeed * -1;  
+  if (x1 > width || x1 < 0) {
+    x1Speed = x1Speed * -1;  
   }
-  x = x + xSpeed; // adding a negative xSpeed *decreases* x, right?
+  x1 = x1 + x1Speed; // adding a negative xSpeed *decreases* x, right?
   
   // same as above, but for the top and bottom of the canvas
-  if (y > width || y < 0) {
-    ySpeed = ySpeed * -1;  
+  if (y1 > width || y1 < 0) {
+    y1Speed = y1Speed * -1;  
   }
-  y = y + ySpeed;
+  y1 = y1 + y1Speed;
 
-  ellipse(x, y, d);
+  ellipse(x1, y1, d1);
 }
